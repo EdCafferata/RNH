04907f666b8a7e8e74d8b4e1cee7f5308a05abe8
Ed Cafferata <Ed Cafferata>
2021-12-05 22:10:11 +0100

    Nieuwe versie inclusief Apple Watch :-)

---
+++ RnhGpxTracker/ObjectiveCBridgingHeader.h
@@ -0,0 +1,10 @@
+//  BridgeToObjectiveC.h
+//  RnhGpxTracker
+//  Created by merlos on 14/09/14.
+//
+#ifndef RnhGpxTracker_ObjectiveCBridgingHeader_h
+#define RnhGpxTracker_ObjectiveCBridgingHeader_h
+// Bridge to be able to import Objective-C Libraries into Swift
+// Tutorial https://medium.com/@stigi/swift-cocoapods-da09d8ba6dd2
+//#import "GPX.h"
+#endif
