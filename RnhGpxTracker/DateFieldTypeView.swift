20ca04303ffa6e92d8241ba01e667aae83e5eb6f
EdCafferata <cafferata@live.nl>
2021-12-06 21:04:56 +0100

    .

--- RnhGpxTracker/DateFieldTypeView.swift
+++ RnhGpxTracker/DateFieldTypeView.swift
@@ -1,34 +1,26 @@
-//
 //  DateFieldTypeView.swift
-//  OpenGpxTracker
+//  RnhGpxTracker
 //
 //  Created by Vincent Neo on 15/4/20.
-//
-
+//  Updated by Ed Cafferata 2021/12/06.
 import UIKit
-
 /// View that is meant to be attached above keyboard, supplementing default name inputs.
 @available(iOS 9.0, *)
 class DateFieldTypeView: UIScrollView {
-    
     // MARK: Localization Strings
     private let kSingleDigit = NSLocalizedString("SINGLE_DIGIT", comment: "")
     private let kFull = NSLocalizedString("FULL_TEXT", comment: "")
     private let kText = NSLocalizedString("TEXT", comment: "")
-    
     private let kOfMonth = NSLocalizedString("OF_MONTH", comment: "")
     private let kOfYear = NSLocalizedString("OF_YEAR", comment: "")
-    
     private let kAbbr = NSLocalizedString("ABBR_GMT", comment: "")
     private let kUTCoffset = NSLocalizedString("UTC_OFFSET", comment: "")
     private let kGMTshort = NSLocalizedString("GMT_SHORT", comment: "")
     private let kGMTfull = NSLocalizedString("GMT_FULL", comment: "")
     private let kLocation = NSLocalizedString("LOCATION", comment: "")
     private let kLocationTime = NSLocalizedString("LOCATION_TIME", comment: "")
-    
     /// date formatter to display current time example
     private let dateFormatter = DateFormatter()
-    
     /// valid date fields that are to be displayed.
     private var dateFields: [DateField] {
         var fields = [DateField]()
@@ -87,7 +79,6 @@ class DateFieldTypeView: UIScrollView {
                                             "ZZZZ": kGMTfull,
                                             "VVV": kLocation,
                                             "VVVV": kLocationTime]))
-
         return fields
     }
     /// Default initializer
@@ -95,13 +86,11 @@ class DateFieldTypeView: UIScrollView {
         super.init(frame: frame)
         viewDidInit()
     }
-    
     /// Default initializer
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         viewDidInit()
     }
-    
     /// Things to do, when view successfully inits.
     func viewDidInit() {
         if #available(iOS 13.0, *) {
@@ -109,7 +98,6 @@ class DateFieldTypeView: UIScrollView {
         } else {
             self.backgroundColor = .lightKeyboard
         }
-        
         // holds everything
         let scrollStack = UIStackView()
         scrollStack.axis = .horizontal
@@ -117,19 +105,16 @@ class DateFieldTypeView: UIScrollView {
         scrollStack.alignment = .leading
         scrollStack.spacing = 25
         scrollStack.translatesAutoresizingMaskIntoConstraints = false
-        
         for field in dateFields {
             let vStack = genVStack(field: field)
             scrollStack.addArrangedSubview(vStack)
         }
-        
         self.addSubview(scrollStack)
         self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[sStack]-20-|",
                                                            options: .alignAllLeft,
                                                            metrics: nil,
                                                            views: ["sStack": scrollStack]))
     }
-    
     /// Generates vertical stack that encapsulates text title, with all date patterns of same type.
     ///
     ///     |TYPE|
@@ -142,20 +127,15 @@ class DateFieldTypeView: UIScrollView {
         vStack.alignment = .leading
         vStack.spacing = 0
         vStack.translatesAutoresizingMaskIntoConstraints = false
-        
         let textTitle = UIInsetLabel()
         textTitle.text = field.type.uppercased()
         textTitle.textColor = .gray
         textTitle.font = .boldSystemFont(ofSize: 14)
         textTitle.insets = UIEdgeInsets(top: 5, left: 5, bottom: 0, right: 0)
-        
         vStack.addArrangedSubview(textTitle)
         vStack.addArrangedSubview(genHStack(field: field))
-        
         return vStack
-        
     }
-    
     /// Generates horizontal stack that encapsulates all date patterns of same type.
     ///
     ///     |pattern|pattern|pattern|
@@ -168,29 +148,23 @@ class DateFieldTypeView: UIScrollView {
         hStack.alignment = .leading
         hStack.spacing = 10
         hStack.translatesAutoresizingMaskIntoConstraints = false
-        
         for pattern in field.patterns {
             let button = DatePatternButton(type: .custom)
             button.layer.cornerRadius = 10
-
             dateFormatter.dateFormat = pattern
             let currentDateExample = dateFormatter.string(from: Date())
             button.setTitle(currentDateExample, for: .normal)
             button.titleLabel?.text = currentDateExample
             button.contentEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
-            
             // color of button font
             if #available(iOS 13, *) {
                 button.setTitleColor(.blackAndWhite, for: .normal)
             } else {
                 button.setTitleColor(.black, for: .normal)
             }
-            
             // for passing pattern to textField when needed
             button.pattern = pattern
-            
             button.addTarget(self, action: #selector(buttonTapped(sender:)), for: .touchUpInside)
-
             // Subtitle implementation (optional)
             if let subtitle = field.subtitles?[pattern] {
                 hStack.addArrangedSubview(genSubtitleView(subtitle: subtitle, button: button))
@@ -198,10 +172,8 @@ class DateFieldTypeView: UIScrollView {
                 hStack.addArrangedSubview(button)
             }
         }
-        
         return hStack
     }
-    
     /// Generates subtitle view for genHStack(field:)
     ///
     ///        |pattern|pattern|pattern|
@@ -215,12 +187,10 @@ class DateFieldTypeView: UIScrollView {
         subVStack.alignment = .leading
         subVStack.spacing = -2.5
         subVStack.translatesAutoresizingMaskIntoConstraints = false
-        
         subtitleLabel.insets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         subtitleLabel.text = subtitle.uppercased()
         subtitleLabel.font = .boldSystemFont(ofSize: 8)
         subtitleLabel.textAlignment = .center
-        
         subVStack.addArrangedSubview(button)
         subVStack.addArrangedSubview(subtitleLabel)
         let subtitleConstraint = NSLayoutConstraint(item: subtitleLabel,
@@ -231,20 +201,15 @@ class DateFieldTypeView: UIScrollView {
                                                     multiplier: 1,
                                                     constant: 0)
         subtitleConstraint.isActive = true
-        
         return subVStack
     }
-    
     /// Called when any pattern button is tapped.
     @objc func buttonTapped(sender: DatePatternButton) {
         NotificationCenter.default.post(name: .dateFieldTapped, object: nil, userInfo: ["sender": sender.pattern])
     }
-    
 }
 /// Notifications name of for date pattern sending
 extension Notification.Name {
-    
     /// When date field type is tapped from view.
     static let dateFieldTapped = Notification.Name("dateFieldTapped")
-    
 }
