e01d9334a49a32a393169d76458f047f0dbdf2c8
EdCafferata <cafferata@live.nl>
2021-12-06 22:56:33 +0100

    .

--- RnhGpxTracker-Watch Extension/ExtensionDelegate.swift
+++ RnhGpxTracker-Watch Extension/ExtensionDelegate.swift
@@ -1,31 +1,25 @@
-//
 //  ExtensionDelegate.swift
-//  OpenGpxTracker-Watch Extension
-//
+//  RnhGpxTracker-Watch Extension
+
 //  Created by Vincent on 5/2/19.
+//  Update by Ed Cafferata 2021/12/06.
 //  Copyright © 2019 TransitBox. All rights reserved.
 //
-
 import WatchKit
-
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
-
     func applicationDidFinishLaunching() {
         // Perform any final initialization of your application.
     }
-
     func applicationDidBecomeActive() {
         // Restart any tasks that were paused (or not yet started) while the application was inactive.
         // If the application was previously in the background, optionally refresh the user interface.
     }
-
     func applicationWillResignActive() {
         // Sent when the application is about to move from active to inactive state.
         // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message)
         // or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, etc.
     }
-
     func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
         // Sent when the system needs to launch the application in the background to process tasks.
         // Tasks arrive in a set, so loop through and process each one.
@@ -56,5 +50,4 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
             }
         }
     }
-
 }
